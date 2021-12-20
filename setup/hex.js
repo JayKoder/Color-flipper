cf45af64efe72c42bceb470d3f09a3ea47803f49
Stephen <stephenojo48@yahoo.com>
2021-12-20 12:01:04 +0100

    Update hex.js

--- setup/hex.js
+++ setup/hex.js
@@ -3,6 +3,15 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-    // get random number between 0 - 3
-    const randomNumber = 2;
-})
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * hex.length);
+}
